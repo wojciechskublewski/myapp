c3c56d955c83c25421b51db17cef4b3624ef5ce1
wojciech.skublewski@wp.pl <wojciech.skublewski@wp.pl>
2019-01-06 09:37:18 +0100

    zadania cd

--- src/main/java/pl/coderslab/Session/Sess01Del.java
+++ src/main/java/pl/coderslab/Session/Sess01Del.java
@@ -5,15 +5,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "Sess01Del")
+@WebServlet("/Sess01Del")
 public class Sess01Del extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("counter");
+        response.getWriter().append("Counter skasowany");
 
     }
 }
--- src/main/java/pl/coderslab/Session/Sess01Get.java
+++ src/main/java/pl/coderslab/Session/Sess01Get.java
@@ -5,15 +5,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "Sess01Get")
+@WebServlet("/Sess01Get")
 public class Sess01Get extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Object counter = session.getAttribute("counter");
 
+        if (counter==null) {
+            response.getWriter().append("brak counter sesji");
+        } else {
+            int counterInt = (int) counter;
+            response.getWriter().append("wartosc counter: ").append(String.valueOf(counterInt));
+            session.setAttribute("counter",++counterInt);
+        }
     }
 }
